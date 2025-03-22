2b846e2a516243f76ef6cec3c35c1152e0b38a36
idanharari11 <idanharari112@gmail.com>
2025-03-22 05:27:31 +0200

    Add script loading for NagishLiBar: implement useEffect to dynamically load and position the NagishLiBar script for enhanced user experience.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import Navbar from '@/components/Navbar';
 import { DarkModeProvider } from '@/context/DarkModeContext';
 import '../app/globals.css';
 import Footer from '@/components/Footer';
+import { useEffect } from 'react';
 
 const heebo = Heebo({
   subsets: ['hebrew'],
@@ -12,6 +13,30 @@ const heebo = Heebo({
 });
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "/nagishli.js"; // הנתיב לקובץ שהעתקת ל-public
+    script.async = true;
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      setTimeout(() => {
+        const nagishLiBar = document.querySelector("#NagishLiBar");
+        if (nagishLiBar) {
+          nagishLiBar.style.position = "fixed";
+          nagishLiBar.style.bottom = "20px";
+          nagishLiBar.style.right = "20px";
+          nagishLiBar.style.left = "auto";
+          nagishLiBar.style.zIndex = "9999";
+          nagishLiBar.style.top = "auto";
+        }
+      }, 1000);
+    };
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <div className={`${heebo.className} overflow-x-hidden`}>
       <Head>
