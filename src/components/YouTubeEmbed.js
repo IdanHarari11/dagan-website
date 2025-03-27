cb8deca158294ef75bcb9f88fd833a7424086890
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-03-28 00:52:39 +0200

    עדכון רכיב YouTubeEmbed: שינוי גובה ה-iframe לעיצוב גמיש יותר, עדכון מרווחים לשיפור הנראות והנגישות של התוכן.

--- src/components/YouTubeEmbed.js
+++ src/components/YouTubeEmbed.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const YouTubeEmbed = ({ videoUrl }) => {
   return (
-    <div className="flex justify-center my-8">
-      <div className="w-full max-w-3xl aspect-w-16 aspect-h-9 h-[25rem]">
+    <div className="flex justify-center my-4 sm:my-8 px-4 sm:px-0">
+      <div className="w-full max-w-3xl aspect-w-16 aspect-h-9 h-[15rem] sm:h-[25rem]">
         <iframe
           className="w-full h-full rounded-lg shadow-[0_0_15px_rgba(59,130,246,0.5)]"
           src={videoUrl}
