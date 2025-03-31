306c0fe9fc5cca642a39c9fa177dac19f8fe8be6
idanharari11 <idanharari112@gmail.com>
2025-03-31 15:57:54 +0300

    עדכון רכיבי Testimonials: תיקון שמות קבצי תמונות לשיפור אחידות ושיפור תיאורי התוכן. הוספת כותרת לדף הראשי ושיפור תגיות מטא לדירוג במנועי חיפוש.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -40,6 +40,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <div className={`${heebo.className} overflow-x-hidden`}>
       <Head>
+        <title>תכנית דגן - מנהיגות מגשרת לעתיד טוב יותר</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
       <DarkModeProvider>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,21 +6,24 @@ export default function Document() {
       <Head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
         <link rel="icon" href="/favicon.ico" />
+        
+        {/* Basic Meta Tags */}
         <meta name="description" content="תכנית מנהיגות ייחודית המחברת בין עולמות ומפתחת מנהיגים צעירים למען עתיד טוב יותר" />
         <meta name="keywords" content="מנהיגות, תכנית דגן, עתיד טוב יותר, מנהיגים צעירים" />
+        
+        {/* Open Graph Meta Tags */}
         <meta property="og:title" content="תכנית דגן - מנהיגות מגשרת לעתיד טוב יותר" />
         <meta property="og:description" content="תכנית מנהיגות ייחודית המחברת בין עולמות ומפתחת מנהיגים צעירים למען עתיד טוב יותר" />
         <meta property="og:image" content="/images/dagan.jpg" />
         <meta property="og:url" content="https://yourwebsite.com" />
+        <meta property="og:site_name" content="תכנית דגן - מנהיגות מגשרת לעתיד טוב יותר"/>
+        <meta property="og:locale" content="he_IL" />
+        
+        {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="תכנית דגן - מנהיגות מגשרת לעתיד טוב יותר" />
         <meta name="twitter:description" content="תכנית מנהיגות ייחודית המחברת בין עולמות ומפתחת מנהיגים צעירים למען עתיד טוב יותר" />
         <meta name="twitter:image" content="/images/dagan.jpg" />
-        <meta property="og:site_name" content="תכנית דגן - מנהיגות מגשרת לעתיד טוב יותר"/>
-        <meta property="og:locale" content="he_IL" />
-        <title>תכנית דגן - מנהיגות מגשרת לעתיד טוב יותר</title>
-        <meta name="description" content="תכנית מנהיגות ייחודית המחברת בין עולמות ומפתחת מנהיגים צעירים למען עתיד טוב יותר" />
-        <meta name="keywords" content="מנהיגות, תכנית דגן, עתיד טוב יותר, מנהיגים צעירים" />
       </Head>
       <body>
         <Main />
