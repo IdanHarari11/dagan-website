91abf17f95999de9097b73ea582c32ad25201330
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-14 17:20:36 +0300

    הוספת תמיכה ב-Google Tag Manager לקובץ layout.js לשיפור המעקב והניתוח של נתונים, כולל הוספת תגית noscript עבור משתמשים ללא JavaScript.

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="he" dir="rtl">
       <head>
-        {/* Google tag (gtag.js) */}
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GT-PZMQJZ9J');
+            `,
+          }}
+        />
+        {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-MBE6MPSZMJ"
           strategy="afterInteractive"
@@ -31,7 +45,18 @@ export default function RootLayout({ children }) {
           }}
         />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GT-PZMQJZ9J"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   )
 } 
