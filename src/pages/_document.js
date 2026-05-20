64968a05fc03223feec47f5d2ce9316b37fe94b7
Your Name <idanharari112@gmail.com>
2026-05-20 17:40:49 +0300

    Update meta tags in _document.js for improved SEO: Revised descriptions for general, Open Graph, and Twitter tags to reflect the program's focus on ethical leadership and community collaboration.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,12 +9,12 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
         
         {/* Basic Meta Tags */}
-        <meta name="description" content="תכנית מנהיגות ייחודית המחברת בין עולמות ומפתחת מנהיגים צעירים למען עתיד טוב יותר" />
+        <meta name="description" content="תכנית דגן למנהיגות - מנהיגות ערכית לבניית אמון, שיתופי פעולה והובלת שינוי בחברה הישראלית" />
         <meta name="keywords" content="מנהיגות, תכנית דגן, עתיד טוב יותר, מנהיגים צעירים" />
         
         {/* Open Graph Meta Tags */}
         <meta property="og:title" content="תכנית דגן למנהיגות" />
-        <meta property="og:description" content="תכנית מנהיגות ייחודית המחברת בין עולמות ומפתחת מנהיגים צעירים למען עתיד טוב יותר" />
+        <meta property="og:description" content="תכנית דגן למנהיגות - מנהיגות ערכית לבניית אמון, שיתופי פעולה והובלת שינוי בחברה הישראלית" />
         <meta property="og:image" content="/images/dagan.jpg" />
         <meta property="og:url" content="https://yourwebsite.com" />
         <meta property="og:site_name" content="תכנית דגן למנהיגות"/>
@@ -23,7 +23,7 @@ export default function Document() {
         {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="תכנית דגן למנהיגות" />
-        <meta name="twitter:description" content="תכנית מנהיגות ייחודית המחברת בין עולמות ומפתחת מנהיגים צעירים למען עתיד טוב יותר" />
+        <meta name="twitter:description" content="תכנית דגן למנהיגות - מנהיגות ערכית לבניית אמון, שיתופי פעולה והובלת שינוי בחברה הישראלית" />
         <meta name="twitter:image" content="/images/dagan.jpg" />
 
         {/* Google Tag Manager */}
